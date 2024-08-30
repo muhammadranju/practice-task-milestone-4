7da1047484adc6bf65ddce855cda6a4ef1dc343c
Muhammad Ranju <mdranju23@gmail.com>
2024-08-30 09:58:44 +0600

    update index file

--- web_day_1/index.js
+++ web_day_1/index.js
@@ -3,4 +3,49 @@ var b = 2;
 var c = 3;
 var d = 4;
 var e = 5;
-console.log(a + b + c + d + e);
+// console.log(a + b + c + d + e);
+
+// console.log(10 + "10");
+
+// console.log(12 % 5);
+
+// if (true) console.log("true");
+
+const isLogin = true;
+(function () {
+  //   if (isLogin) console.log("you are logged in");
+})();
+
+// isLogin
+//   ? console.log("you are logged in")
+//   : console.log("you are not logged in");
+
+// console.log(10 < "5");
+
+var num = 10;
+if (num > 15) {
+  //   console.log("Greater than 15");
+} else if (num > 5) {
+  //   console.log("Greater than 5 but less than or equal to 15");
+} else {
+  //   console.log("Less than or equal to 5");
+}
+
+// console.log(6 != 6);
+
+// let x = 5;
+// if (x < 10) {
+//   x = x + 3;
+// } else {
+//   x = x - 2;
+// }
+// console.log(x);
+
+let score = 75;
+let grade;
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+}
+console.log(grade);
