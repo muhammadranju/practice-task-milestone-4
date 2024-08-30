219fc1a6c3b2e379b77ac6c6587a74ad146ca01d
Muhammad Ranju <mdranju23@gmail.com>
2024-08-30 22:27:32 +0600

    update index.js file

--- web_day_1/index.js
+++ web_day_1/index.js
@@ -1,8 +1,8 @@
-var a = 1;
-var b = 2;
-var c = 3;
-var d = 4;
-var e = 5;
+// var a = 1;
+// var b = 2;
+// var c = 3;
+// var d = 4;
+// var e = 5;
 // console.log(a + b + c + d + e);
 
 // console.log(10 + "10");
@@ -11,10 +11,10 @@ var e = 5;
 
 // if (true) console.log("true");
 
-const isLogin = true;
-(function () {
-  //   if (isLogin) console.log("you are logged in");
-})();
+// const isLogin = true;
+// (function () {
+//   //   if (isLogin) console.log("you are logged in");
+// })();
 
 // isLogin
 //   ? console.log("you are logged in")
@@ -22,14 +22,14 @@ const isLogin = true;
 
 // console.log(10 < "5");
 
-var num = 10;
-if (num > 15) {
-  //   console.log("Greater than 15");
-} else if (num > 5) {
-  //   console.log("Greater than 5 but less than or equal to 15");
-} else {
-  //   console.log("Less than or equal to 5");
-}
+// var num = 10;
+// if (num > 15) {
+//   console.log("Greater than 15");
+// } else if (num > 5) {
+//   console.log("Greater than 5 but less than or equal to 15");
+// } else {
+//   console.log("Less than or equal to 5");
+// }
 
 // console.log(6 != 6);
 
@@ -41,11 +41,46 @@ if (num > 15) {
 // }
 // console.log(x);
 
-let score = 75;
-let grade;
-if (score >= 90) {
-  grade = "A";
-} else if (score >= 80) {
-  grade = "B";
-}
-console.log(grade);
+// let score = 75;
+// let grade;
+// if (score >= 90) {
+//   grade = "A";
+// } else if (score >= 80) {
+//   grade = "B";
+// }
+// console.log(grade);
+
+// const price = 200;
+// const discount = 10;
+// if (price >= 5000) {
+//   const discountPrice = (price * discount) / 100;
+//   const payAmount = price - discountPrice;
+//   console.log(
+//     `Discounted amount is  ${discountPrice} and pay Amount is ${payAmount}
+//   `
+//   );
+// } else {
+//   console.log(`Price is ${price}`);
+// }
+
+// console.log(isNaN(NaN));
+// const xe = {};
+
+// console.log(!!xe);
+
+const x = 3;
+// const y = 6;
+// const z = 2.3;
+// const result = x + y + z;
+
+// const divided = x / z;
+// const value = Number(divided.toFixed(4));
+// console.log(value + x);
+// console.log(value);
+
+// console.log(x ** 3);
+
+// console.log(10 / 3);
+// console.log(8 % 10);
+
+console.log(isNaN("10"));
