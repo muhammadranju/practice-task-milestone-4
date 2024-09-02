22125255aefa3fc3c9add54c52a58ab9b4464069
Muhammad Ranju <mdranju23@gmail.com>
2024-09-02 06:39:20 +0600

    added for loop

--- web_day_1/nested.js
+++ web_day_1/nested.js
@@ -1,4 +1,4 @@
-const money = -10;
+// const money = -10;
 // if (money > 300) {
 //   console.log("Bor you are rich!!");
 // } else {
@@ -14,26 +14,126 @@ const money = -10;
 // }
 
 // using ternary operator
-let price = 500;
-const isLeader = false;
-const payPrice = (price = !isLeader ? price + 100 : 0);
-// console.log(payPrice);
+// let price = 500;
+// const isLeader = false;
+// const payPrice = (price = !isLeader ? price + 100 : 0);
+// // console.log(payPrice);
 
-if (!isLeader) {
-  price = price + 100;
-} else {
-  price = 0;
-}
+// if (!isLeader) {
+//   price = price + 100;
+// } else {
+//   price = 0;
+// }
+// // console.log(price);
+
+// if (isLeader) {
+//   if (price > 500) {
+//     price = price / 2;
+//   } else {
+//     price = 0;
+//   }
+// } else {
+//   price = price = +1000;
+// }
+
 // console.log(price);
 
-if (isLeader) {
-  if (price > 500) {
-    price = price / 2;
-  } else {
-    price = 0;
+// using shortcut not operator
+
+// const isLeader = true;
+
+// if (!isLeader) {
+//   console.log("vitore dukte dibi na");
+// } else {
+//   console.log("leader ascha! chair charo");
+// }
+
+// const isPassed = true;
+// if (!isPassed) {
+//   console.log("you are failed");
+// } else {
+//   console.log("you are passed");
+// }
+
+// const myArr = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6],
+// ];
+// const newArr = myArr.flat();
+// console.log(newArr[0]);
+
+// let text = "How - are you doing - today?";
+
+// const newArr = text.split("-");
+
+// console.log(newArr.join(""));
+
+// console.log(newArr);
+
+// const newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 0;
+
+// const new2 = newArr.forEach((i) => {
+//   sum += i;
+// });
+
+// console.log(new2);
+// console.log(sum);
+
+// const myArr = newArr.map((i) => {
+//   console.log(i);
+// });
+
+// console.log(myArr);
+// for (let i = 0; i < newArr.length; i++) {
+//   sum += newArr[i];
+//   //   console.log(i + 2);
+// }
+
+// let i = 0;
+// while (i <= newArr.length) {
+//   console.log(i);
+//   i++;
+// }
+// console.log(sum);
+
+// for (let i = 100; i <= 100; i--) {
+//   console.log(i);
+
+//   if (i === 0) break;
+// }
+
+// even number
+// let start = 1;
+// let end = 20;
+// for (let i = start; i <= end; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`Even Numbers: ${i}`);
+//   }
+// }
+
+// // odd number
+// for (let i = start; i <= end; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(`Odd Numbers: ${i}`);
+//   }
+// }
+
+// let total = 0;
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 3 === 0) {
+//     console.log(i);
+//     total += i;
+//   }
+// }
+// console.log(total);
+
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 1) {
+    continue;
   }
-} else {
-  price = price = +1000;
+  console.log(i);
 }
 
-console.log(price);
+console.log(5 % 5);
