0d90ce0f226fad260f76b771f55432306e5b36fc
Muhammad Ranju <mdranju23@gmail.com>
2024-09-08 09:06:09 +0600

    feat: how to sum of price in array of object in real life project problem

--- function/problems.js
+++ function/problems.js
@@ -2,23 +2,42 @@
 
 // console.log(Math.min(...heights));
 
-const phones = [
-  { id: 1, name: "Samsung", price: 20000, camera: "12mp", color: "Black" },
-  { id: 1, name: "Xoami", price: 18000, camera: "12mp", color: "Black" },
-  { id: 1, name: "Oppo", price: 27000, camera: "12mp", color: "Black" },
-  { id: 1, name: "Vivo", price: 30000, camera: "12mp", color: "Black" },
-  { id: 1, name: "Iphone", price: 50000, camera: "12mp", color: "Black" },
-];
+// const phones = [
+//   { id: 1, name: "Samsung", price: 20000, camera: "12mp", color: "Black" },
+//   { id: 1, name: "Xoami", price: 18000, camera: "12mp", color: "Black" },
+//   { id: 1, name: "Oppo", price: 27000, camera: "12mp", color: "Black" },
+//   { id: 1, name: "Vivo", price: 30000, camera: "12mp", color: "Black" },
+//   { id: 1, name: "Iphone", price: 50000, camera: "12mp", color: "Black" },
+// ];
 
 // this is real life problem you can do it on real project
-function getLowerPrice(phones) {
-  let min = phones[0];
-  for (const phone of phones) {
-    if (phone.price < min.price) {
-      min = phone;
-    }
+// function getLowerPrice(phones) {
+//   let min = phones[0];
+//   for (const phone of phones) {
+//     if (phone.price < min.price) {
+//       min = phone;
+//     }
+//   }
+//   return min;
+// }
+// const min = getLowerPrice(phones);
+// console.log(min);
+
+// this problem is real life problem, how to sum of price in array of object in real life project
+const products = [
+  { name: "laptop", price: 300 },
+  { name: "mobile", price: 100 },
+  { name: "mouse", price: 700 },
+  { name: "keyboard", price: 1200 },
+];
+
+function getShopping(products) {
+  let total = 0;
+  for (const product of products) {
+    total = total + product.price;
   }
-  return min;
+  return total;
 }
-const min = getLowerPrice(phones);
-console.log(min);
+
+const total = getShopping(products);
+console.log(total);
